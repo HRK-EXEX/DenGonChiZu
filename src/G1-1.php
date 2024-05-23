63907725f81995251db33468f0cc0a619ae2e384
rikuto <rikuriku.shin@gmail.com>
2024-05-23 10:58:19 +0900

    情報取得の微調整

--- src/G1-1.php
+++ src/G1-1.php
@@ -37,7 +37,7 @@
         }
 
         public function fetchPosts() {
-            $query = 'SELECT p.*, u.user_name, u.user_icon FROM Posts p JOIN Users u ON p.user_id = u.user_id ORDER BY p.date DESC';
+            $query = 'SELECT p.user_id, p.content, p.image, p.comment_count, p.like_count, u.user_name FROM Posts p JOIN Users u ON p.user_id = u.user_id ORDER BY p.date DESC';
             $stmt = $this->db->prepare($query);
             $stmt->execute();
             return $stmt->fetchAll(PDO::FETCH_OBJ);
