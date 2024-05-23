3d6f37e07a45dfebb12285079706b3af4a31d77e
rikuto <rikuriku.shin@gmail.com>
2024-05-23 10:43:36 +0900

    接続DB処理を一時的に分離

--- src/G1-1.php
+++ src/G1-1.php
@@ -12,8 +12,21 @@
 <div id="sidebar-container"></div>
 
 <div id="content">
+    
     <?php
-    require_once 'php/db.php';
+    class Database {
+        private $conn;
+    
+        public function connect() {
+            try {
+                $this->conn = new PDO('mysql:host=mysql305.phy.lolipop.lan;dbname=LAA1517436-linedwork;charset=utf8', 'LAA1517436', 'hyperBassData627');
+                $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            } catch (PDOException $e) {
+                echo 'Connection Error: ' . $e->getMessage();
+            }
+            return $this->conn;
+        }
+    }
 
     class Post {
         private $db;
