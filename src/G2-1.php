5bdd85b207277f1d814214d4b844f7ab8eba5b20
HRK.EXEX <74143200+HRK-EXEX@users.noreply.github.com>
2024-05-24 10:27:21 +0900

    逆手に取ってみる(G2-1)

--- src/G2-1.php
+++ src/G2-1.php
@@ -16,21 +16,19 @@
     <link rel="stylesheet" href="css/G2-1.css">
 </head>
 <body>
-    <form class="parent" method="POST">
-        <div class="main-part">
+    <div class="main-part">
+        <form id="newing" class="parent" method="POST">
             <div class="method">新規投稿</div>
             <input name="post_title" class="box-base title" placeholder="投稿タイトルを入力..." required value="<?=$title?>">
             <div class="box-base image-box">
                 <input type="file" name="post_img" accept="image/jpg, image/jpeg, image/png, image/webp" value="<?=$image?>"/>
             </div>
             <textarea name="post_text" class="box-base content" placeholder="本文を入力..." required><?=$text?></textarea>
-        </div>
-        <div class="operation">
-            <a href="G1-1.html" class="button-base">
-                <button type="button" class="button-base back">戻る</button>
-            </a>
-            <button type="submit" class="button-base proceed">投稿</button>
-        </div>
-    </form>
+        </form>
+    </div>
+    <div class="operation">
+        <button onclick="location.href='G2-2.php'" class="button-base back">戻る</button>
+        <button type="submit" form="newing" class="button-base proceed">投稿</button>
+    </div>
 </body>
 </html>
