87313affbbdec45eed70c6f8c07870eb5a705f1c
HRK.EXEX <74143200+HRK-EXEX@users.noreply.github.com>
2024-06-03 09:53:51 +0900

    なんちゃってphpを実装(G2-3, G2-4)

--- src/G2-4.php
+++ src/G2-4.php
@@ -1,3 +1,49 @@
+<?php
+    require 'php/db.php';
+    $postId = $_GET["post_id"] ?? null;
+    $userId = $_GET["user_id"] ?? null;
+    $posted = $_POST["posted"] ?? false;
+    $mes = "投稿編集";
+
+    // 投稿の存在確認
+    if(isset($postId)) {
+        try {
+            $sql = $db -> query("SELECT * FROM Posts WHERE post_id = $postId AND user_id = $userId");
+            $res = $sql -> fetch(PDO::FETCH_ASSOC);
+
+            $title = $_POST['post_title'] ?? $res['title'] ?? null;
+            $image = $_POST['post_img'] ?? $res['img_path'] ?? null;
+            $text = $_POST['post_text'] ?? $res['content'] ?? null;
+        } catch (PDOException $e) {
+            $title = $text = "exception occured";
+        }
+
+        // 削除ボタンが押されたかの確認＆投稿処理
+        if ($posted) {
+            $date = date("Y-m-d H:i:s");
+            // $mes = $date . "\n";
+            // $mes .= print_r($_FILES, true) . "\n";
+            // $mes .= print_r($_POST, true) . "\n";
+            // $mes .= print_r($_SESSION, true) . "\n";
+
+            // SQL挿入部
+            $sql = $db -> query(
+                "UPDATE Posts SET
+                    title = $title,
+                    content = $text,
+                    img_path = $target,
+                    'date' = $date
+                WHERE post_id = $postId AND user_id = $userId"
+            );
+            $res = $sql -> fetch(PDO::FETCH_ASSOC);
+
+            // リダイレクト
+            if (isset($res))
+                header("Location: G1-1.php");
+        }
+    } else $nullified = true;
+?>
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -9,19 +55,31 @@
 </head>
 <body>
     <div id="sidebar-container"></div>
-    <?php include 'side.php'; ?>
-    
-    <div class="parent">
-        <div class="main-part">
-            <div class="method">以下の投稿を削除しようとしています。<br>本当に実行しますか？</div>
-            <div class="box-base title">投稿タイトル</div>
-            <div class="box-base image-box"><img class="image" src="../img/NoImage.png"></div>
-            <div class="box-base content">くぁｗせｄｒｆｔｇｙふじこｌｐくぁｗせｄｒｆｔｇｙふじこｌｐくぁｗせｄｒｆｔｇｙふじこｌｐ</div>
-        </div>
-        <div class="operation">
-            <button onclick="location.href='G2-2.php'" class="button-base back">戻る</button>
-            <button onclick="location.href='G1-1.php'" class="button-base delete">削除</button>
-        </div>
-    </div>
+    <?php
+        include 'side.php';
+
+        if (!$nullified) {
+            echo '
+                <div class="parent">
+                    <form id="modify" class="main-part" method="POST">
+                        <div class="method">以下の投稿を削除しようとしています。<br>本当に実行しますか？</div>
+                        <div class="box-base title">'.$title.'</div>
+                        <div class="box-base image-box">';
+                        if (isset($res['img_path'])) {
+                            if ($res['img_path']) {;
+                                echo '<img name="post_img" class="image" src="../img/'.$image.'.png">';
+                            } else echo '<img name="post_img" class="image" src="../img/NoImage.png">';
+                        }
+                    echo '</div>
+                        <div class="box-base content">'.$text.'</div>
+                    </form>
+                    <div class="operation">
+                        <button onclick="location.href=\'G2-2.php\'" class="button-base back">戻る</button>
+                        <button type="submit" form="modify" class="button-base delete">削除</button>
+                    </div>
+                </div>
+            ';
+        } else echo '<h3>このポストは利用できません。</h3>';
+    ?>
 </body>
 </html>
