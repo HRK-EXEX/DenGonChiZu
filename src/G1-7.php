cf87d02c6a62d73ac7e85d93c595732ab7ae735e
uno-2 <148303462+uno-2@users.noreply.github.com>
2024-06-05 12:12:30 +0900

    G1-7

--- src/G1-7.php
+++ src/G1-7.php
@@ -12,12 +12,12 @@ session_destroy();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ログアウト画面</title>
-    <link rel="stylesheet" href="css/G1-7.css">
+    <link rel="stylesheet" href="css/G-7.css">
 </head>
 <body>
     <div class="container">
         <div class="message">ログアウトしました</div>
-        <button onclick="location.href='index.html'">TOP画面に戻る</button>
+        <button onclick="location.href='G1-1.php'">TOP画面に戻る</button>
     </div>
 </body>
 </html>
