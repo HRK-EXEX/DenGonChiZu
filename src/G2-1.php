21301b04d70a8e6228c0c6e6ba182756e01c749d
HRK.EXEX <74143200+HRK-EXEX@users.noreply.github.com>
2024-06-07 10:10:58 +0900

    画像アップロード機能追加（プロトタイプ）+α

--- src/G2-1.php
+++ src/G2-1.php
@@ -9,33 +9,50 @@
     $image = $_FILES['post_img'] ?? null;
     $text = $_POST['post_text'] ?? null;
     $target = $image['name'] ?? null;
+    $error = null;
 
     if (empty($target)) $target = 'null';
 
     $mes = "新規投稿";
 
     // 投稿ボタンが押されたかの確認＆投稿処理
-    if ($posted) {
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $date = date("Y-m-d H:i:s");
         // $mes = $date . "\n";
         // $mes .= print_r($_FILES, true) . "\n";
         // $mes .= print_r($_POST, true) . "\n";
         // $mes .= print_r($_SESSION, true) . "\n";
 
-        // SQL挿入部
         try {
+            // ファイルが既に存在するかチェック
+            if (file_exists($target_file)) {
+                $error = "アップロードされたファイルは既に存在します。";
+            }
+
+            // SQL挿入部
             $userId = $_SESSION['user']['user_id'];
-            $str = "INSERT INTO Posts VALUE (null, $userId, '$title', '$text', $target, '$date', 0)";
+            $uploadPath = isset($image) ? 'img/posts/'.$postId.'-'.$target : null;
+
+            $str = "INSERT INTO Posts VALUE (null, $userId, '$title', '$text', $uploadPath, '$date', 0)";
             
             $sql = $db -> query($str);
             $res = $sql -> fetch(PDO::FETCH_ASSOC);
-        } catch (PDOException $e) {
+
+            // 画像送信部
+            if (isset($uploadPath)) {
+                if (!move_uploaded_file($_FILES['post_img']['tmp_name'], $uploadPath)) {
+                    $error = "ファイルのアップロードに失敗しました。";
+                };
+            }
+
+            // リダイレクト
+            if (isset($res))
+                header("Location: G1-1.php");
+        } catch (Exception $e) {
             $title = $text = 'exception occured: '.$e->getMessage().'<br>'.$str;
         }
 
-        // リダイレクト
-        if (isset($res))
-            header("Location: G1-1.php");
+        if ($error) $title = $text = 'error occured: '.$error.'<br>';
     }
 ?>
 
@@ -54,7 +71,6 @@
 
     <div class="parent">
         <form id="newing" class="main-part" method="post" enctype="multipart/form-data">
-            <input type="hidden" name="posted" value="true">
             <div class="method"><?=$mes?></div>
             <input name="post_title" class="box-base title" placeholder="投稿タイトルを入力..." required value="<?=$title?>">
             <div class="box-base image-box">
