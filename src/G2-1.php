9269c5f97e661aa6674d1ee80582b04d69693f5c
HRK.EXEX <74143200+HRK-EXEX@users.noreply.github.com>
2024-06-10 10:24:42 +0900

    まさかとは思いますがここまで必要？

--- src/G2-1.php
+++ src/G2-1.php
@@ -47,14 +47,14 @@
 
                 $target = basename($img_name);
 
-                $uploadPath = isset($target) ? '/img/posts/'.$postId.'-'.$target : null;
+                $uploadPath = isset($target) ? '/web/linedWork/img/posts/'.$postId.'-'.$target : null;
 
                 $str = "UPDATE Posts SET img_path = '$uploadPath' WHERE post_id = ".$postId;
 
                 // 画像送信部
                 if (isset($uploadPath)) {
                     $res2 = $db -> query($str) -> fetch(PDO::FETCH_ASSOC);
-                    if ($_FILES['post_img']['error'] !== UPLOAD_ERR_OK) {
+                    if ($_FILES['post_img']['error'] !== 0) {
                         if (!move_uploaded_file($img_name_tmp, $uploadPath)) {
                             $error = "ファイルのアップロードに失敗しました。";
                         }
