79e68009155cb679afbeee6b7c1d20ec4f145816
HRK.EXEX <74143200+HRK-EXEX@users.noreply.github.com>
2024-06-24 10:56:30 +0900

    G2-3 よくわかりません

--- src/G2-3.php
+++ src/G2-3.php
@@ -45,7 +45,7 @@
             // SQL変更部
             try {
                 // まずは画像ファイルの確認をし、ファイル名を確定
-                $target = !empty($img_name) && $img_name == $res['img_path'] ? basename($img_name) : null;
+                $target = !empty($img_name) && $img_name != $res['img_path'] ? basename($img_name) : null;
                 $uploadPath = (!empty($target) && !$deleteImg) ? '../img/posts/'.$target : null;
 
                 // 内容を更新
