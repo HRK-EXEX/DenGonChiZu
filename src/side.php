b073ea91e5f679510fbb73cb66978adfd7f688a0
HRK.EXEX <74143200+HRK-EXEX@users.noreply.github.com>
2024-06-24 10:11:44 +0900

    サイドバーの画像をログイン状況に応じて更新

--- src/side.php
+++ src/side.php
@@ -7,11 +7,19 @@
     <title>サイドバー</title>
 </head>
 <body>
+    <?php
+        $userId = $_SESSION['user']['user_id'] ?? null;
+    ?>
     <div class="sidebar">
         <img src="../img/home.png" alt="画像1" onclick="window.location.href='G1-1.php';">
         <img src="../img/icon.png" alt="画像2" onclick="window.location.href='G3-1.php?myFollow=true';">
         <img src="../img/haguruma.png" alt="画像3" onclick="window.location.href='G4-1.php';">
-        <img src="../img/login.png" alt="画像4" onclick="window.location.href='G1-7.php';">
+
+        <?php if (isset($userId)): ?>
+            <img src="../img/logout.png" alt="画像4" onclick="window.location.href='G1-7.php';">
+        <?php else: ?>
+            <img src="../img/login.png" alt="画像4" onclick="window.location.href='G1-6.php';">
+        <?php endif; ?>
         
     </div>
 </body>
